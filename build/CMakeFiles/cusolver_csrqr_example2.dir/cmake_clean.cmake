b2e2b068be8200049bca6d883bb6286092640bae
zhengwupal <zhengwupal@163.com>
2022-03-18 16:46:29 +0800

    update cmakelists

---
+++ build/CMakeFiles/cusolver_csrqr_example2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cusolver_csrqr_example2.dir/src/cusolver_csrqr_example2.cu.o"
+  "CMakeFiles/cusolver_csrqr_example2.dir/src/cusolver_csrqr_example2.cu.o.d"
+  "cusolver_csrqr_example2"
+  "cusolver_csrqr_example2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/cusolver_csrqr_example2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/test_allocator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_allocator.dir/src/test_allocator.cu.o"
+  "CMakeFiles/test_allocator.dir/src/test_allocator.cu.o.d"
+  "test_allocator"
+  "test_allocator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/test_allocator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
